17932bafa8268f70b01b804526da54c51d612e07
Aguiaxxx <149949468+Aguiaxxx@users.noreply.github.com>
2025-12-23 16:22:07 -0300

    Create Cm.py

---
+++ Cm.py
@@ -0,0 +1,6 @@
+sudo apt update
+sudo apt install python3 python3-pip -y
+pip3 install playwright
+python3 -m playwright install chromium
+pip3 install pyautogui
+sudo apt install python3-tk python3-dev scrot -y
